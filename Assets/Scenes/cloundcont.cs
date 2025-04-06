977eee39705d9de85e4b8c45d72210305c5c6505
DomasDM <96250566+DomasDM@users.noreply.github.com>
2025-04-06 11:15:01 +0300

    f
    fg

--- Assets/Scenes/cloundcont.cs
+++ Assets/Scenes/cloundcont.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class cloundcont : MonoBehaviour
 {
+    public Transform[] fruit;
+    static public string spawnedYet = "n";
+    static public Vector2 cloudxPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class cloundcont : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        spawnFruit();
         if (Input.GetKey("a"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-2, 0);
@@ -26,5 +31,22 @@ public class cloundcont : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
+
+        cloudxPos = transform.position;
+    }
+
+    void spawnFruit()
+    { 
+        if (spawnedYet=="n")
+        {
+            StartCoroutine(spawntimer());
+
+            spawnedYet = "y";
+        }
+    }
+    IEnumerator spawntimer()
+    {
+        yield return new WaitForSeconds(.75f);
+        Instantiate(fruit[Random.Range(0, 6)], transform.position, fruit[0].rotation);
     }
 }
--- Assets/Scenes/fruit.cs
+++ Assets/Scenes/fruit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string inthecloud = "y";
     void Start()
     {
         
@@ -13,6 +13,22 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (inthecloud=="y")
+        {
+            GetComponent<Transform>().position = cloundcont.cloudxPos;
+        }
+        if(Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            inthecloud = "n";
+            cloundcont.spawnedYet = "n";
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == gameObject.tag)
+        {
+            Destroy(gameObject);
+        }
     }
 }
